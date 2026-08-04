24da3c4f21f138bb422aeba2c2058e6034e5168d
Aditya waidande 03 <waidandeaditya@gmail.com>
2026-08-04 18:33:02 +0530

    Time: 3 ms (21.91%), Space: 19.2 MB (93.08%) - LeetHub

---
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+
+        ans = ""
+
+        for word in words:
+            ans += word[0]
+
+        return ans == s
