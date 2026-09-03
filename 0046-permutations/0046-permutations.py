7d2a09324867544503f7b065404b339e6aa54702
Aditya waidande 03 <waidandeaditya@gmail.com>
2026-09-03 14:20:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.14%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def backtrack(path, options):
+            if not options:
+                res.append(path)
+                return
+            for i in range(len(options)):
+                backtrack(path + [options[i]], options[:i] + options[i+1:])
+                
+        backtrack([], nums)
+        return res
+        
